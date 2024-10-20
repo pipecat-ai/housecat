e1ccd6aa879535566d305bc894059e7a7513d8ef
vipyne <vipyne@gmail.com>
2024-10-20 16:03:59 -0500

    basic button
    pos absolute

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <!-- src/App.vue -->
 <template>
   <div id="app">
+    <ExportJson ref="exportJson" @click="onButtonClick"/>
     <GraphEditor ref="graphEditor" />
   </div>
 </template>
@@ -8,17 +9,26 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import GraphEditor from './components/GraphEditor.vue';
+import ExportJson from './components/ExportJson.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
+    ExportJson,
     GraphEditor
+  },
+  methods: {
+    onButtonClick() {
+      alert('Export JSON Button clicked!');
+    }
   },
   setup() {
     const graphEditor = ref<InstanceType<typeof GraphEditor> | null>(null);
+    const exportJson = ref<InstanceType<typeof ExportJson> | null>(null);
 
     return {
-      graphEditor
+      graphEditor,
+      exportJson
     };
   }
 });
